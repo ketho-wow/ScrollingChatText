c47664180d4682d062de3d152607506c742bac3f
Ketho <ngsc1@live.cn>
2013-05-23 03:06:45 +0200

    Bumped TOC to 50300

--- ScrollingChatText.lua
+++ ScrollingChatText.lua
@@ -2,7 +2,7 @@
 --- Author: Ketho (EU-Boulderfist)		---
 --- License: Public Domain				---
 --- Created: 2011.07.05					---
---- Version: 0.8.0 [2013.04.27]			---
+--- Version: 0.9.0 [2013.05.23]			---
 -------------------------------------------
 --- Curse			http://www.curse.com/addons/wow/scrollingchattext
 --- WoWInterface	http://www.wowinterface.com/downloads/info20827-ScrollingChatText.html
@@ -18,7 +18,7 @@
 -- # LibSink(?) Messages with Links sometimes not even being output to a chat channel
 
 local NAME, S = ...
-S.VERSION = "0.8.0"
+S.VERSION = GetAddOnMetadata(NAME, "Version")
 S.BUILD = "Release"
 
 -- ScrollingChatText abbreviates to SCR in order to avoid confusion with SCT (ScrollingCombatText)
